671e8be8b426783d26057366a9156be6b418b3eb
Einer Fabian Aponte Cubides <eaponte@heinsohn.com.co>
2024-02-23 08:52:23 -0500

    Se avanza a dia 12

---
+++ src/js/dia/diaOnce.js
@@ -0,0 +1,21 @@
+const dia10 = document.querySelector('.container-11');
+
+const animacionDiaOnce = () => {
+    const contenedor = document.querySelector('.container-11__zapatos');
+    const verticalUno = document.querySelector('.container-11__zapatos-der');
+    const verticalDos = document.querySelector('.container-11__zapatos-izq');
+
+    dia10.addEventListener('mouseover', () => {
+        verticalUno.classList.add('left');
+        verticalDos.classList.add('right');
+        contenedor.classList.add('opacity');
+    })
+
+    dia10.addEventListener('mouseout', () => {
+        verticalUno.classList.remove('left');
+        verticalDos.classList.remove('right');
+        contenedor.classList.remove('opacity');
+    })
+}
+
+export default animacionDiaOnce;
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import '../styles/app.scss'
-import agregarAnimacion from './dia/diaDiez.js' 
+import agregarAnimacion from './dia/diaDiez.js';
+import animacionDiaOnce from './dia/diaOnce.js';
 
 agregarAnimacion();
+animacionDiaOnce();
